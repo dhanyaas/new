0f9e76af042389069fab5e4cad69c2222e87fd5f
Dhanyashree A <98949368+dhanyaas@users.noreply.github.com>
2022-05-31 16:19:50 +0530

    Update 7b.sh

--- 7b.sh
+++ 7b.sh
@@ -1,1 +1,23 @@
 
+if [ $# -ge 2 ]
+then
+	for fn in $*
+	do
+		if [ -e $fn ]
+		then 
+			if [ $fn = $1 ]
+			then
+				continue
+			fi
+			for w in `cat $1`
+			do
+				lc=`grep -woi "$w" $fn | wc -l`
+				echo "$w is $lc in $fn"
+			done
+		else
+			echo "$fn File not Found!"
+		fi
+	done
+else
+	echo "ParameterPattern: PatternFile FilesToCheck.. "
+fi
