85295e11aa3c949f38a57781bf9ed839d689f46f
Dhanyashree A <98949368+dhanyaas@users.noreply.github.com>
2022-05-31 16:22:11 +0530

    Update 6a.sh

--- 6a.sh
+++ 6a.sh
@@ -1,1 +1,32 @@
 
+if [ $# -ge 1 ]
+then
+	if [ ! -e ~/mydir ]
+	then
+		mkdir ~/mydir
+	fi	
+	for k in $*
+	do	
+			if [ -f $k ]
+			then
+				find $HOME -name "$k">files
+				while read line
+				do
+			
+					num=`echo $line | grep -o "/" | wc -l`
+					num=`expr $num + 1`
+					file=`echo $line | cut -d "/" -f $num`
+					cat $line
+					echo "--------------------------------------------------------------"
+					if [ `ls ~/mydir | grep $file | wc -l` -eq 0 ]
+					then
+						cp $line ~/mydir 
+					fi
+				done<files
+			else
+				echo "File doesn't exists"
+			fi
+	done
+else
+	echo "Give arguments"
+fi
