58ef4e7612e4ef3c548ee341c24dd7f77b4949f4
Dhanyashree A <98949368+dhanyaas@users.noreply.github.com>
2022-05-31 17:33:08 +0530

    Update 3a.sh

--- 3a.sh
+++ 3a.sh
@@ -1,1 +1,23 @@
+if [ -e $1 ]
 
+then
+
+  	set -- `ls -ld $1`
+
+	echo "Permission are :$1" 
+
+	echo "File links count is: $2"
+
+	echo "User name is :$3"
+
+	echo "Group name is: $4"
+
+	echo "File Size is: $5"
+
+	echo "Modification is done $6 $7 at $8"
+
+ else 
+
+	echo "File does not exit"
+
+fi
