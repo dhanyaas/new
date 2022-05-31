68a20e03f1aaa8513edcc4db4dc0ed528b302dd4
Dhanyashree A <98949368+dhanyaas@users.noreply.github.com>
2022-05-31 17:34:17 +0530

    Update 3b.sh

--- 3b.sh
+++ 3b.sh
@@ -1,1 +1,63 @@
+  
 
+echo "Enter the password"
+
+stty -echo
+
+read p1
+
+stty echo
+
+echo "Confirm the password"
+
+stty -echo
+
+read p2
+
+stty echo
+
+while [ "$p1" != "$p2" ]
+
+do
+
+echo "Password doesnot match Re-enter the password"
+
+stty -echo
+
+read p2
+
+stty echo
+
+done
+
+clear
+
+echo "Locked"
+
+echo "Enter the password to unlock:"
+
+stty -echo
+
+read p3
+
+stty echo
+
+while [ "$p1" != "$p3" ]
+
+do
+
+clear
+
+echo "Terminal Locked"
+
+echo "Password doesnot match Re-enter the password"
+
+stty -echo
+
+read p3
+
+stty echo
+
+done
+
+echo "Terminal Unlock"
