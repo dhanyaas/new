6ddaf13afbd44f5e7caccab79aa892084ae36f2e
Dhanyashree A <98949368+dhanyaas@users.noreply.github.com>
2022-05-31 16:20:25 +0530

    Update 7a.sh

--- 7a.sh
+++ 7a.sh
@@ -1,1 +1,28 @@
 
+set -- `who`
+t=$4
+u=`whoami`
+for i in $*
+do
+	if [ $u != $1 ]
+	then 
+		`shift 5`
+	else
+		t=$4
+		
+	fi
+done
+set -- `echo $t | tr ":" " "`
+h=$1
+if [ $h -ge 4 ] && [ $h -lt 12 ]
+then
+echo " good morning $u"
+elif [ $h -ge 12 ] && [ $h -lt 16 ]
+then
+echo " good afternoon $u "
+elif [ $h -ge 16 ] && [ $h -lt 19 ]
+then
+echo " good evening $u "
+else
+echo " good night $u "
+fi
