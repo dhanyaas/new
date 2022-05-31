4fcbbc1b1e49d21d9624edfa95b2f56c4b5a3d3b
Dhanyashree A <98949368+dhanyaas@users.noreply.github.com>
2022-05-31 16:15:51 +0530

    Update 9a.sh

--- 9a.sh
+++ 9a.sh
@@ -1,1 +1,18 @@
 
+if [ $# -eq 3 ]
+then
+    if [ -f $1 ]
+      then 
+        l=`cat $1 | wc -l`
+        if [ $2 -gt $3 -a $2 -gt $l -a $3 -gt $2 ]
+          then
+           head -n $1 $3 | tail -n  +$2
+           else 
+               echo "Enter avalid arguments"
+           fi
+        else
+          echo "File does not exits"
+         fi
+ else
+  echo "Enter the no of arguments"
+ fi     
