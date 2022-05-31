caa46178880ac4be603bfbd6a74000259b9d1de2
Dhanyashree A <98949368+dhanyaas@users.noreply.github.com>
2022-05-31 16:23:08 +0530

    Update 4b.sh

--- 4b.sh
+++ 4b.sh
@@ -1,1 +1,30 @@
 
+if [ $# -ge 1 ]
+then
+	if [ $# -eq 2 ]
+	then
+		sd=$2
+	else
+		sd="."
+	fi
+	
+	file=$1
+	if [ -f $file ]
+	then
+		
+		set -- `ls -i $file`
+		inode=$1
+		tfiles=`find $sd -inum $inode | wc -l`
+		if [ $tfiles -eq 1 ]
+		then
+			echo "$file has no link files"
+		else
+			find $sd -inum $inode
+			echo "The inode number for $tfiles files is $inode"
+		fi
+	else
+		echo "Give valid file"
+	fi
+else
+	echo "Give atleast one arguement"
+fi
